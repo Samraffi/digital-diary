debe27fb7ac8fa8b08bd39b8470b81c0bca11ee7
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-04-30 14:56:20 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-800">
+      <h1 className="text-2xl font-handwriting">Мой дневник</h1>
+      <p className="text-center mt-4">Здесь будет ваша запись...</p>
+    </div>
+  );
+}
+
+export default App;
